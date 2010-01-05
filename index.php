7424f7e8de652724a3b8fa1d32fbf00ddb1d7ac8
Joel Lehtonen <jopesale@jalava.cc.jyu.fi>
2010-01-05 11:58:39 +0200

    A simple PHP for splitting a prefix from a phone number.
    Still needs heavy work.

---
+++ index.php
@@ -0,0 +1,40 @@
+<pre>
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+require("tmp_prefixes.php");
+print("jep".$max_length);
+
+//print_r($GLOBALS);
+
+if (empty($_GET['n'])) {
+  print("numero puuttuu. TODO parempi sivu.");
+  exit(0);
+}
+
+$number = $_GET['n'];
+
+print("numero annettu\n");
+
+$match = false;
+
+foreach($prefixes as $prefix) {
+  if (!strncmp($number, $prefix, strlen($prefix))) {
+    // Is matching this prefix
+    $match=true;
+    break;
+  }
+}
+
+if ($match==false) {
+  print("ei osunut numero ".$number."\n");
+  exit(0);
+ }
+
+$postfix=substr($number,strlen($prefix));
+
+print("Osui ja upposi: operaattori on ".$prefix.", numero on ".$postfix."\n");
+
+?>
+</pre>
